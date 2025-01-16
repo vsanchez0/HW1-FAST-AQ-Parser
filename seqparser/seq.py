bee1bf8e6528490ba34e49e91f426f88aa2b761d
vsanchez0 <valeria.sanchez21@my.stjohns.edu>
2025-01-15 16:56:00 -0800

    write transcription and rev transcription function

--- seqparser/seq.py
+++ seqparser/seq.py
@@ -7,16 +7,19 @@ ALLOWED_NUC = TRANSCRIPTION_MAPPING.keys()
 
 def transcribe(seq: str, reverse: bool = False) -> str:
     """
-    Write a function that will transcribe (replace DNA sequence to RNA
+    Function that will transcribe (replace DNA sequence to RNA
     by replacing all 'T' to 'U') in an input sequence
     """
-    pass
+    rna_seq = seq.replace('T','U')
+    if reverse:
+        rna_seq = rna_seq[::-1]
+    return rna_seq
 
 def reverse_transcribe(seq: str) -> str:
     """
     Write a function that will transcribe an input sequence and reverse
     the sequence
     """
-    # Hey this is my comment
-    # Again!
-    pass
+    rna_seq = seq.replace('T','U')
+    rev_rna_seq = rna_seq[::-1]
+    return rev_rna_seq
